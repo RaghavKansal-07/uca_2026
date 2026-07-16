1cbb56115d863309298dd0e9e3fdea6a20ef3544
Raghav Kansal <raghavkansal2007@gmail.com>
2026-07-16 16:21:35 +0000

    Added reverse string solution

---
+++ reverse_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void reverse_string(char *str) {
+    int length = 0;
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[] = "Data Structures";
+
+    reverse_string(str);
+
+    printf("%s\n", str);
+
+    return 0;
+}
